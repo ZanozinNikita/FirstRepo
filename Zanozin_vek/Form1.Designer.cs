d78b88c647d9509f5ff24f047c0e89ae780ea097
ZanozinNikita <hilipoas@mail.ru>
2012-12-18 11:58:24 +0600

    draw_line

--- Zanozin_vek/Form1.Designer.cs
+++ Zanozin_vek/Form1.Designer.cs
@@ -28,22 +28,52 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.RCross = new System.Windows.Forms.RadioButton();
+            this.RLine = new System.Windows.Forms.RadioButton();
             this.SuspendLayout();
+            // 
+            // RCross
+            // 
+            this.RCross.AutoSize = true;
+            this.RCross.Location = new System.Drawing.Point(402, 326);
+            this.RCross.Name = "RCross";
+            this.RCross.Size = new System.Drawing.Size(59, 17);
+            this.RCross.TabIndex = 0;
+            this.RCross.TabStop = true;
+            this.RCross.Text = "RCross";
+            this.RCross.UseVisualStyleBackColor = true;
+            // 
+            // RLine
+            // 
+            this.RLine.AutoSize = true;
+            this.RLine.Location = new System.Drawing.Point(402, 350);
+            this.RLine.Name = "RLine";
+            this.RLine.Size = new System.Drawing.Size(53, 17);
+            this.RLine.TabIndex = 1;
+            this.RLine.TabStop = true;
+            this.RLine.Text = "RLine";
+            this.RLine.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(499, 385);
+            this.Controls.Add(this.RLine);
+            this.Controls.Add(this.RCross);
             this.Name = "Form1";
             this.Text = "Form";
             this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
             this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseDown);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.RadioButton RCross;
+        private System.Windows.Forms.RadioButton RLine;
     }
 }
 
--- Zanozin_vek/Form1.cs
+++ Zanozin_vek/Form1.cs
@@ -10,10 +10,16 @@ using System.IO;
 
 namespace Zanozin_vek
 {
+    public abstract class Shape
+    {
+        public abstract void DrawWith(Graphics g, Pen pen);
+    }
     public partial class Form1 : Form
     {
         public Pen pMain = new Pen(Color.Black);
-        List<Cross> Shapes = new List<Cross>();
+        List<Shape> Shapes = new List<Shape>();
+        public bool isShapeStart = true;
+        public Point ShapeStart = new Point();
         public Form1()
         {
             InitializeComponent();
@@ -21,28 +27,61 @@ namespace Zanozin_vek
         private void Form1_MouseDown(object sender, MouseEventArgs e)
         {
             this.Text = Convert.ToString(e.X) + " " + Convert.ToString(e.Y);
-            Shapes.Add(new Cross(e.Location));
-            Refresh();
+            if (RCross.Checked)
+            {
+                Shapes.Add(new Cross(e.Location));
+                isShapeStart = true;
+                Refresh();
+            }
+            if (RLine.Checked)
+            {
+                if (isShapeStart == true)
+                {
+                    ShapeStart.X = e.X;
+                    ShapeStart.Y = e.Y;
+                    isShapeStart = false;
+                }
+                else
+                {
+                    Shapes.Add(new Line(ShapeStart,e.Location));
+                    isShapeStart = true;
+                    Refresh();
+                }
+            }
         }
         private void Form1_Paint(object sender, PaintEventArgs e)
         {
-            foreach (Cross p in this.Shapes)
+            foreach (Shape p in this.Shapes)
             {
                 p.DrawWith(e.Graphics, pMain);
             }
         }
     }
-    public class Cross
+    public class Cross : Shape
     {
         Point F;
         public Cross(Point _F)
         {
             F = _F;
         }
-        public void DrawWith(Graphics g, Pen pen)
+        public override void DrawWith(Graphics g, Pen pen)
         {
             g.DrawLine(pen, F.X - 2, F.Y - 2, F.X + 2, F.Y + 2);
             g.DrawLine(pen, F.X + 2, F.Y - 2, F.X - 2, F.Y + 2);
+        }
+    }
+    public class Line : Shape
+    {
+        Point S;
+        Point F;
+        public Line(Point _S, Point _F)
+        {
+            this.S = _S;
+            this.F = _F;
+        }
+        public override void DrawWith(Graphics g, Pen pen)
+        {
+            g.DrawLine(pen, this.S.X, this.S.Y, this.F.X, this.F.Y);
         }
     }
 }
